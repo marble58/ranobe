32c9a41cb08eb2da3698957b5828fe226c7940dd
Dmitry <dimjkeee58@yandex.ru>
2021-10-07 11:43:42 +0300

    0.01

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/marble58/ranobe
 go 1.17
 
 require (
-	github.com/andybalholm/brotli v1.0.2 // indirect
-	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.30.0 // indirect
+	github.com/valyala/quicktemplate v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
-//go:generate go get -u github.com/valyala/quicktemplate/qtc
+// The following line is needed for generating go code from templates
+// with `go generate`.
+// See https://blog.golang.org/generate for more info.
+// Quicktemplate compiler (qtc) must be installed before running
+// `go generate`:
+//
+//     go get -u github.com/valyala/quicktemplate/qtc
+//
 //go:generate qtc -dir=templates
-
 package main
 
 import (
